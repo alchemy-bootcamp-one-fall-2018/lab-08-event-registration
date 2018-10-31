c335152f723a02324db092085a3e4e8f4bb3120e
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-31 16:26:19 -0700

    registration form is complete

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import registrationForm from './registration-form.js';
+import eventApi from './event-api.js';
+
 
 registrationForm.init();
 
--- registration-form.js
+++ registration-form.js
@@ -2,30 +2,35 @@ const form = document.getElementById('event-form');
 
 const elements = form.elements;
 
-
-
-
 const registrationForm = {
 
-    init() {
+    init(onAdd) {
         
         form.addEventListener('submit', function(event) {
             event.preventDefault();
-            const finalForm = {};
-            finalForm.name = elements.name.value;
-            return finalForm;
-            
 
+            const guestDetail = {};
 
+            //Name and Contact
+            guestDetail.name = elements.name.value;
+            guestDetail.pronoun = elements.pronoun.value;
+            guestDetail.email = elements.email.value;
+
+            //T-shirt
+            guestDetail.size = elements.size.value;
+            guestDetail.style = elements.style.value;
+            guestDetail.color = elements.color.value;
+
+            //Number of guests
+            guestDetail.bringing = elements.bringing.value;
+
+            //Meal
+            guestDetail.meal = elements.meal.value;
+
+            onAdd(guestDetail);
+            form.reset();
         });
-
-
-
-
-
-
     }
-
 };
 
 export default registrationForm;
