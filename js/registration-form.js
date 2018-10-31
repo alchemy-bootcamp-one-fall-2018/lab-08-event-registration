146763684195802405da65da9f1fbfe90e6aa4dc
avanvalk <drewvv@gmail.com>
2018-10-31 15:58:16 -0700

    added constants to registration-form.js

---
+++ js/registration-form.js
@@ -0,0 +1,10 @@
+
+const form = document.getElementById('event-registration');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const pronoun = document.getElementById('pronoun');
+const shirtSize = document.getElementById('shirt-size');
+const shirtStyle = document.getElementById('shirt-style');
+const shirtColor = document.getElementById('shirt-color');
+const guests = document.getElementById('guests');
+const meal = document.getElementById('meal');
