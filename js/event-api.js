9cd7b595da4e720e42b472bb57e60d2b998c1a16
Cheri A Portman <cheriportman@hotmail.com>
2018-10-31 14:48:13 -0700

    Api is created

---
+++ js/event-api.js
@@ -0,0 +1,15 @@
+let items = [];
+
+function saveItems() {
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+const itemsApi = {
+    add(item) {
+        items.push(item);
+        saveItems();
+    }
+};
+
+
+export default itemsApi;
