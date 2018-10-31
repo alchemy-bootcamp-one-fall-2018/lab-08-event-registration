d9e263de11b717d8cd0fe5dee9b0cc53816d1ee5
Marcy Silverman <marcysilve225@gmail.com>
2018-10-31 15:04:02 -0700

    html and files set up

---
+++ js/registration-form.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form-registration');
+const emailForm = document.getElementById('email');
+const phoneForm = document.getElementById('phone'); 
+const mediaForm = document.getElementById('social-media'); 
+
+const itemForm = {
+    init(onAdd) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            const elements = forms.elements; 
+        }
+    }
+}
