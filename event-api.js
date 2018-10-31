ebdbd4892aefd5fbb53754bd5318f7ea0e53bbbf
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-31 15:28:55 -0700

    set up api

---
+++ event-api.js
@@ -0,0 +1,14 @@
+const guests = [];
+
+function saveGuests() {
+    localStorage.setItem('guests', JSON.stringify(guests));
+}
+
+const guestsApi = {
+    add(guest) {
+        guests.push(guest);
+        saveGuests();
+    }
+};
+
+export default guestsApi;
