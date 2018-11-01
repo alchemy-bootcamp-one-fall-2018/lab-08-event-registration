0a75849c1073a222b1da2dde7a6bbb898e5d6691
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-31 17:26:26 -0700

    wires up attendee's pronouns and shirt data

--- js/registration-form.js
+++ js/registration-form.js
@@ -15,8 +15,15 @@ const itemForm = {
             //data object
             const attendee = {};
 
+            //set up the attendee's name and email
             attendee.firstName = elements['first-name'].value;
             attendee.lastName = elements['last-name'].value;
+            attendee.email = elements.email.value;
+
+            //set up the attendee's pronoun
+            attendee.pronoun = elements.pronoun.value;
+
+            attendee.shirtSize = elements['tshirt-size'].value;
             onAdd(attendee);
         });
     }
