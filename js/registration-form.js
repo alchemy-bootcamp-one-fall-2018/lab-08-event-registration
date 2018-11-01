d6d1d6298f80fc58a30ffd24d4229d0e2aabf48a
bpkotowski <bighoopee@gmail.com>
2018-10-31 18:20:59 -0700

    some errors need fixing

--- js/registration-form.js
+++ js/registration-form.js
@@ -1,25 +1,48 @@
-//const form = document.getElementById('attendee-registration');
+const form = document.getElementById('attendee-registration');
 const yesGuests = document.getElementById('yes');
 const noGuests = document.getElementById('no');
-////const howMany = document.getElementById('how-many');
+
+//const howMany = document.getElementById('how-many');
 //const shirtSize = document.getElementById();
 //const add more
 
 const registrationForm = {
     init(onAdd) {
-console.log('joe')
+//console.log('joe');
         yesGuests.addEventListener('change', function() {
             yesGuests = true;
         });
 
         noGuests.addEventListener('change', function(){
-            noGuests = true;
+            //noGuests = true;
         });
+
+        howMany
     },
 };
 
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const elements = form.elements;
+    const partyPeeps = {};
+    partyPeeps.name = elements.name.value;
+    
+    partyPeeps.guest = elements.guest.value === 'yes';
+    if(partyPeeps.guest) {
+        const count = parseInt(elements.guest.value);
+        if(!isNaN(count)) {
+            partyPeeps.guest = count;
+        }
+    }
+
+    onAdd(partyPeeps);
+
+});
 
 
 
 
-export default registrationForm
+
+
+export default registrationForm;
