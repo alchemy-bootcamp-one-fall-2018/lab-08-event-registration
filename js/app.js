4cc17bb7b4ec032b4f6e4feee3fe099489d7db47
Heather <unphurlng@gmail.com>
2018-11-01 08:51:05 -0700

    on registration-form.js form values pushing to guestInfo object and going to console.

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ import guestsApi from './event-api.js';
 import registrationForm from './registration-form.js';
 
 
-guestsApi.add('testing');
+
+registrationForm.init(function(banana) {
+    guestsApi.add('testing', banana);
+});
+
 // registrationForm.;
