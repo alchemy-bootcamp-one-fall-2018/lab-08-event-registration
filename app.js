694741c86ee9946005306948ef5cc87e2c7c27f3
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-01 08:13:52 -0700

    split css files and moved js into folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import eventsApi from './events-api.js';
-import eventForm from './event-form.js';
-import eventList from './event-list.js';
+import eventsApi from './js/events-api.js';
+import eventForm from './js/event-form.js';
+import eventList from './js/event-list.js';
 
 eventForm.init(function(event) {
     eventsApi.add(event);
