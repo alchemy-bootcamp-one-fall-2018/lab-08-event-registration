b5fa962f4ab5e72f03ce39c4e93702add911c684
Cheri A Portman <cheriportman@hotmail.com>
2018-10-31 18:09:32 -0700

    made the checkboxes work and get into the array!

--- js/registration-form.js
+++ js/registration-form.js
@@ -4,8 +4,14 @@ const malePronoun = document.getElementById('he-him');
 const nongenPronoun = document.getElementById('they-them');
 const email = document.getElementById('email');
 const shirttype = document.getElementById('shirttype');
-
-
+const shirtsize = document.getElementById('shirtsize');
+const shirtcolor = document.getElementById('shirtcolor');
+const guests = document.getElementById('guests');
+const chicken = document.getElementById('chicken');
+const vegetarian = document.getElementById('vegetarian');
+const glutenfree = document.getElementById('glutenfree');
+const nuts = document.getElementById('nuts');
+const other = document.getElementById('other');
 
 
 const itemForm = {
@@ -13,17 +19,45 @@ const itemForm = {
 
         form.addEventListener('submit', function(event) {
             event.preventDefault();                        //prevents flashing thing
-            const elements = form.elements;
-
-            const attendee = {};
+            const elements = form.elements;                //form elements collection
+            const attendee = {};                           //data object!!!!
 
         
             
-            attendee.firstName = elements.firstName.value;
+            attendee.firstName = elements.firstName.value;    //gather data
             attendee.lastName = elements.lastName.value;          
             attendee.pronoun = elements.pronoun.value;
             attendee.email = elements.email.value;
             attendee.shirttype = elements.shirttype.value;
+            attendee.shirtsize = elements.shirtsize.value;
+            attendee.shirtcolor = elements.shirtcolor.value;
+            attendee.guests = elements.guests.value;
+            attendee.maindish = elements.maindish.value;
+            attendee.restrictions = elements.restrictions.value;
+
+            attendee.maindish = [];
+
+            for(let i = 0; i < elements.maindish.length; i++) {
+                const maindish = elements.maindish[i];
+                if(maindish.checked) {
+                    attendee.maindish.push(maindish.value);
+                }
+            }
+
+            attendee.restrictions = [];
+            for(let i = 0; i < elements.restrictions.length; i++) {
+                const restrictions = elements.restrictions[i];
+                if(restrictions.checked) {
+                    attendee.restrictions.push(restrictions.value);
+                }
+            }
+                console.log(attendee.restrictions);
+            
+           
+
+
+
+
 
 
 
