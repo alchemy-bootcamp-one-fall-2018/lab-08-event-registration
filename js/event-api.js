051152049239ee45346cd3dad82e6e64f33e3a72
‘Michael <pickup.patterns@gmail.com>
2018-11-02 04:10:08 -0700

    wired up event api to save in Local storage and push detal

---
+++ js/event-api.js
@@ -0,0 +1,13 @@
+
+let details = [];
+
+function formCollect() {
+    localStorage.getFormDetails('details', JSON.stringify(details));
+}
+const eventApi = {
+    add (detail) {
+        details.push(detail);
+        formCollect();
+    }
+};
+export default eventApi;
